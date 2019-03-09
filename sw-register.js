f13211e150f052a0e414c770c0001964cf8ed416
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-09 18:43:10 +0000

    Deploy armujahid/armujahid.me to github.com/armujahid/armujahid.me.git:gh-pages

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/sw.js?v=1532164388').then(function(reg) {
+            navigator.serviceWorker.register('/sw.js?v=1552156976').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-                        importScripts("/assets/js/workbox-v3.3.1/workbox-sw.js");
-            workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.3.1"});
+                        importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
+            workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"2936c37194ad943778ba19f407627577"},{"url":"/style.css","revision":"03f0922141dc9d128e91023c2eecddc6"},{"url":"/2018/06/reactnd","revision":"a445932749223bc864549b10e1d16f7e"},{"url":"/2018/02/hacking","revision":"128409be8997fad3948177e02383bcbb"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"c54037f31f476657e15cf9ee545bfe59"},{"url":"/style.css","revision":"03f0922141dc9d128e91023c2eecddc6"},{"url":"/2018/06/reactnd","revision":"a445932749223bc864549b10e1d16f7e"},{"url":"/2018/02/hacking","revision":"128409be8997fad3948177e02383bcbb"}];
             workbox.core.setCacheNameDetails({
     prefix: 'arm-blog',
     suffix: 'v1',

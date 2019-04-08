a20244ac91b5add1f68bf1d67aae28ea389e8184
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-08 06:20:01 +0000

    Deploy armujahid/armujahid.me to github.com/armujahid/armujahid.me.git:gh-pages

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"c54037f31f476657e15cf9ee545bfe59"},{"url":"/style.css","revision":"03f0922141dc9d128e91023c2eecddc6"},{"url":"/2018/06/reactnd","revision":"a445932749223bc864549b10e1d16f7e"},{"url":"/2018/02/hacking","revision":"128409be8997fad3948177e02383bcbb"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"e502f06ab198499b6beabeeb66951728"},{"url":"/style.css","revision":"03f0922141dc9d128e91023c2eecddc6"},{"url":"/2018/06/reactnd","revision":"a445932749223bc864549b10e1d16f7e"},{"url":"/2018/02/hacking","revision":"128409be8997fad3948177e02383bcbb"}];
             workbox.core.setCacheNameDetails({
     prefix: 'arm-blog',
     suffix: 'v1',
